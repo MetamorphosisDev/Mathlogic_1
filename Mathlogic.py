27b56c64bccfc6109378282064cf74e4e7308924
Jona Al Farros <124805962+MetamorphosisDev@users.noreply.github.com>
2025-10-05 23:47:30 +0700

    Update Mathlogic.py

--- Mathlogic.py
+++ Mathlogic.py
@@ -1,1 +1,17 @@
 
+
+from fractions import Fraction
+
+while True :
+    data = input("Masukan Hitungan :")
+    olah = eval(data)
+    print(data, "= ", olah)
+
+    fraksi = Fraction(olah)
+    pecahan = fraksi.limit_denominator()
+    print(data, " = ", pecahan)
+    print("\n")
+#input in terminal(vscode): "10+10" or "10*10" or "10-10" or "10/10"
+#output: "20" , "100" , "0" , "1"
+
+
